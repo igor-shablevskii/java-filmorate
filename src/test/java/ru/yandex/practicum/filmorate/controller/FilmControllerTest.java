20311530a9438e994f379ff7a23f3db1dd4a360a
igor-shablevskii <shablevich@yandex.ru>
2022-07-16 16:03:30 +0700

    add-friends-likes

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FilmControllerTest {
 
-    FilmController controller = new FilmController();
+    FilmController controller;
 
     @Test
     void createValidFilms() {
